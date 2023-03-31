ef7402527119e7f5c683e501f10a844bc8cf0e3f
malik <minubaev@hicebank.ru>
2023-03-31 13:33:44 +0300

    try build

---
+++ aiodistributor/__init__.py
@@ -0,0 +1,9 @@
+def int_or_str(value: str | int) -> int:
+    try:
+        return int(value)
+    except ValueError:
+        return value  # type: ignore
+
+
+__version__ = '0.0.1'
+VERSION = tuple(map(int_or_str, __version__.split('.')))
